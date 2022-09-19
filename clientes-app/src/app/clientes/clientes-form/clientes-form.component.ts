5a64ea236488f9e9bfcfc3ea8bd21f5f35f446a1
Breno Rodrigo Cabral de Oliveira <brcabral@gmail.com>
2022-09-18 23:56:48 -0300

    Melhorando o formulário de clientes

--- clientes-app/src/app/clientes/clientes-form/clientes-form.component.ts
+++ clientes-app/src/app/clientes/clientes-form/clientes-form.component.ts
@@ -22,7 +22,10 @@ export class ClientesFormComponent implements OnInit {
   onSubmit() {
     this.service.salvar(this.cliente).subscribe(response => {
       this.success = true;
+      this.errors = null;
+      this.cliente = response;
     }, errorResponse => {
+      this.success = false;
       this.errors = errorResponse.error.errors;
     });
   }
