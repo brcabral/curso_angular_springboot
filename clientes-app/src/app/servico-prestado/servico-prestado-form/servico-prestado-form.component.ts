7f10fea871f157aa5021e94d5b381422bf68a960
Breno Rodrigo Cabral de Oliveira <brcabral@gmail.com>
2022-09-22 23:23:08 -0300

    Formulario de Serviços prestados parte 1

--- clientes-app/src/app/servico-prestado/servico-prestado-form/servico-prestado-form.component.ts
+++ clientes-app/src/app/servico-prestado/servico-prestado-form/servico-prestado-form.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ClientesService } from 'src/app/clientes.service';
+import { Cliente } from 'src/app/clientes/cliente';
 
 @Component({
   selector: 'app-servico-prestado-form',
@@ -7,10 +9,18 @@ import { Component, OnInit } from '@angular/core';
   ]
 })
 export class ServicoPrestadoFormComponent implements OnInit {
+  clientes: Cliente[];
 
-  constructor() { }
+  constructor(
+    private clienteService: ClientesService) { }
 
   ngOnInit(): void {
+    this.clienteService.getClientes().subscribe(response => {
+      this.clientes = response;
+    });
   }
 
+  onSubmit() {
+    console.log("submit");
+  }
 }
