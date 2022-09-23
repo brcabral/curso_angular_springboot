8c822f2e69caec217a1d03faf0aad304b3a7b6b4
Breno Rodrigo Cabral de Oliveira <brcabral@gmail.com>
2022-09-22 22:46:23 -0300

    Criando o módulo e os componentes de servico prestado

---
+++ clientes-app/src/app/servico-prestado/servico-prestado-form/servico-prestado-form.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-servico-prestado-form',
+  templateUrl: './servico-prestado-form.component.html',
+  styles: [
+  ]
+})
+export class ServicoPrestadoFormComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ clientes-app/src/app/servico-prestado/servico-prestado-lista/servico-prestado-lista.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-servico-prestado-lista',
+  templateUrl: './servico-prestado-lista.component.html',
+  styles: [
+  ]
+})
+export class ServicoPrestadoListaComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
