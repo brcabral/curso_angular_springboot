9458c193fd35f6d94d73b51954492c0060199596
Breno Rodrigo Cabral de Oliveira <brcabral@gmail.com>
2022-09-23 15:58:27 -0300

    Serviços prestados e variaveis de ambiente utilizando Enviroment

---
+++ clientes-app/src/app/servico-prestado.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { ServicoPrestado } from './servico-prestado/servicoPrestado';
+import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ServicoPrestadoService {
+  apiUrl: string = environment.apiUrlBase + '/api/servicos-prestados';
+
+  constructor(
+    private http: HttpClient) { }
+
+  salvar(servicoPrestado: ServicoPrestado): Observable<ServicoPrestado>{
+    return this.http.post<ServicoPrestado>(this.apiUrl, ServicoPrestado);
+  }
+}
