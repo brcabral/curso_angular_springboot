1b4f7ea3a3f4864cf3388ba1bd4ecbe2a30cae30
Breno Rodrigo Cabral de Oliveira <brcabral@gmail.com>
2022-10-11 23:33:36 -0300

    Autenticando com os usuários da base de dados

--- clientes/src/main/java/com/github/brcabral/clientes/config/SecurityConfig.java
+++ clientes/src/main/java/com/github/brcabral/clientes/config/SecurityConfig.java
@@ -1,5 +1,7 @@
 package com.github.brcabral.clientes.config;
 
+import com.github.brcabral.clientes.service.UsuarioService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
@@ -12,12 +14,12 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+    @Autowired
+    private UsuarioService usuarioService;
+
     @Override
     public void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.inMemoryAuthentication()
-                .withUser("breno")
-                .password("123")
-                .roles("USER");
+        auth.userDetailsService(usuarioService).passwordEncoder(passwordEncoder());
     }
 
     @Bean
---
+++ clientes/src/main/java/com/github/brcabral/clientes/service/UsuarioService.java
@@ -0,0 +1,28 @@
+package com.github.brcabral.clientes.service;
+
+import com.github.brcabral.clientes.model.entity.Usuario;
+import com.github.brcabral.clientes.model.repository.UsuarioRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UsuarioService implements UserDetailsService {
+    @Autowired
+    private UsuarioRepository repository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        Usuario usuario = repository.findByUsername(username)
+                .orElseThrow(() -> new UsernameNotFoundException("Usuário não encontrado."));
+
+        return User.builder()
+                .username(usuario.getUsername())
+                .password(usuario.getPassword())
+                .roles("USER")
+                .build();
+    }
+}
