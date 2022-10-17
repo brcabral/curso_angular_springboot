0551ba04c2892e33302717eacc08d74fc85c104f
Breno Rodrigo Cabral de Oliveira <brcabral@gmail.com>
2022-10-16 23:04:00 -0300

    Controlando a Sessão de Usuário com o Token

--- clientes-app/src/app/auth.service.ts
+++ clientes-app/src/app/auth.service.ts
@@ -1,5 +1,6 @@
 import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { JwtHelperService } from '@auth0/angular-jwt';
 import { Observable } from 'rxjs';
 import { environment } from 'src/environments/environment';
 import { Usuario } from './login/usuario';
@@ -12,6 +13,7 @@ export class AuthService {
   tokenUrl: string = environment.apiUrlBase + environment.obterTokenUrl;
   clientId: string = environment.clientId;
   clientSecret: string = environment.clientSecret;
+  jwtHelper: JwtHelperService = new JwtHelperService();
 
   constructor( private http: HttpClient ) { }
 
@@ -33,7 +35,23 @@ export class AuthService {
     return this.http.post(this.tokenUrl, params.toString(), { headers });
   }
 
+  obterToken() {
+    const tokenString = localStorage.getItem('access_token');
+    if (tokenString) {
+      const token = JSON.parse(tokenString).access_token;
+      return token;
+    }
+
+    return null;
+  }
+
   isAuthenticated(): boolean {
+    const token = this.obterToken();
+    if (token) {
+      const expired = this.jwtHelper.isTokenExpired(token);
+      return !expired;
+    }
+
     return false;
   }
 }
