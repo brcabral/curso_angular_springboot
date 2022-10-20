74e2b501fb0296dcef1fbc2bb425acdeba9c5d47
Breno Rodrigo Cabral de Oliveira <brcabral@gmail.com>
2022-10-19 23:16:24 -0300

    Desenvolvendo a API de Contatos

--- agenda/agenda-api/src/main/java/com/github/brcabral/agendaapi/AgendaApiApplication.java
+++ agenda/agenda-api/src/main/java/com/github/brcabral/agendaapi/AgendaApiApplication.java
@@ -1,32 +1,11 @@
 package com.github.brcabral.agendaapi;
 
-import com.github.brcabral.agendaapi.model.entity.Contato;
-import com.github.brcabral.agendaapi.model.repository.ContatoRepository;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class AgendaApiApplication {
-	@Autowired
-	private ContatoRepository repository;
-
-	@Bean
-	public CommandLineRunner commandLineRunner() {
-		return args -> {
-			Contato contato = new Contato();
-			contato.setNome("Nathan Renato Barros");
-			contato.setEmail("nathan_barros@planicoop.com.br");
-			contato.setFavorito(false);
-			repository.save(contato);
-		};
-	}
-
-
 	public static void main(String[] args) {
 		SpringApplication.run(AgendaApiApplication.class, args);
 	}
-
 }
