2be25164b0dc793cc43181a3918e2650441b8eef
Breno Rodrigo Cabral de Oliveira <brcabral@gmail.com>
2022-10-26 23:36:10 -0300

    Melhorias no layout

--- agenda/agenda-app/src/app/app.module.ts
+++ agenda/agenda-app/src/app/app.module.ts
@@ -1,11 +1,16 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http'
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { MatToolbarModule } from '@angular/material/toolbar'
 import { MatInputModule } from '@angular/material/input'
 import { MatButtonModule } from '@angular/material/button'
-import { ReactiveFormsModule } from '@angular/forms';
+
+import { MatTabsModule } from '@angular/material/tabs'
+import { MatTableModule } from '@angular/material/table'
+import { MatIconModule } from '@angular/material/icon'
+import { MatCardModule } from '@angular/material/card'
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -27,6 +32,10 @@ import { ContatoService } from './contato.service';
     MatButtonModule,
     HttpClientModule,
     ReactiveFormsModule,
+    MatTabsModule,
+    MatTableModule,
+    MatIconModule,
+    MatCardModule,
     BrowserAnimationsModule
   ],
   providers: [
